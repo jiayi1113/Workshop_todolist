6b6b3caf171f3314d0570e2d259763bb7b73173c
Elaine Wu <amsmm265@gmail.com>
2021-12-03 13:16:28 +0800

    task_1

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,16 @@
 // TO DO
+// 1. 點擊任務標記是否完成
+//     - 點擊任務 list 可以切換 `checked` class
+
+document.addEventListener("DOMContentLoaded",function(){
+  const list = document.querySelector("ul");
+  list.addEventListener("click", (e) => {
+    console.log(e.target)
+    if(e.target && e.target.nodeName === "LI"){
+      e.target.classList.add("checked");
+    }else{
+      e.target.classList.remove("checked");
+    }
+  })
+});
+
